f948a11208b97832976d538696724e8b3decff97
Nicholas Conn <nicholasjconn@gmail.com>
2023-03-18 11:12:07 -0400

    Human optimized for performance improvements.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from collections import deque
 import os, random, time, math
-from itertools import product
+from itertools import chain, product
 from typing import Tuple
 from colorama import Fore, Style, init
 
@@ -9,14 +10,16 @@ class TankObject:
         self.x, self.y, self.icon, self.color = x, y, icon, color
         self.age = 0
 
-    def find_closest(self, tank_object: list['TankObject']) -> Tuple[int, int]:
-        if not tank_object:
-            return None, None
-        closest = min(tank_object, key=lambda f: max(abs(self.x - f.x), abs(self.y - f.y)))
+    def find_closest(self, grid: list[list['TankObject']], target_class: 'TankObject') -> Tuple[int, int]:
+        closest = None
+        min_distance = float('inf')
+        for obj in chain(*grid):
+            if isinstance(obj, target_class):
+                distance = max(abs(self.x - obj.x), abs(self.y - obj.y))
+                if distance < min_distance:
+                    min_distance = distance
+                    closest = obj
         return closest.x, closest.y
-
-    def get_pos(self) -> Tuple[int, int]:
-        return self.x, self.y
 
     def __str__(self) -> str:
         return self.color + self.icon + Style.RESET_ALL
@@ -25,23 +28,18 @@ class TankObject:
         self.age += 1
 
     def drift_down(self, tank: 'Tank'):
-        possible_moves = [(0, 1), (-1, 1), (1, 1)]
-        occupied_locations = tank.get_occupied_locations()
-
         # Check if there's a rock directly underneath
-        if (self.x, self.y + 1) in tank.get_locations(tank.rocks):
+        if isinstance(tank.grid[self.x][self.y + 1], Rock):
             return
-
-        # Remove occupied locations and positions outside the tank bounds from the possible_moves list
-        unoccupied_moves = [
-            (dx, dy) for dx, dy in possible_moves
-            if 1 <= self.x + dx < tank.width - 1 and 1 <= self.y + dy < tank.height - 1
-            and (self.x + dx, self.y + dy) not in occupied_locations
+        # Move the fish to empty positions
+        possible_moves = [(self.x + dx, self.y + dy) for dx, dy in [(0, 1), (-1, 1), (1, 1)]]
+        possible_moves = [
+            (x, y) for x, y in possible_moves
+            if 1 <= x < tank.width - 1 and 1 <= y < tank.height - 1 and tank.grid[x][y] is None
         ]
-        if unoccupied_moves:
-            dx, dy = random.choice(unoccupied_moves)
-            self.x += dx
-            self.y += dy
+        if possible_moves:
+            tank.move_object(self, *random.choice(possible_moves))
+
 
 class Fish(TankObject):
     def __init__(self, x: int, y: int, icon: str, color: str, adult_age: int = 10, adult_icon: str = None):
@@ -73,31 +71,32 @@ class Fish(TankObject):
 
     def move_toward_target(self, tank: 'Tank', target_x: int, target_y: int):
         possible_moves = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (-1, 1), (-1, -1), (1, -1)]
+        possible_moves = [(self.x + dx, self.y + dy) for dx, dy in possible_moves]
+        possible_moves = [
+            (x, y) for x, y in possible_moves
+            if 1 <= x < tank.width - 1 and 1 <= y < tank.height - 1 and (tank.grid[x][y] is None or isinstance(tank.grid[x][y], Food))
+        ]
 
         best_move = None
         best_distance = float("inf")
-        occupied_locations = tank.get_occupied_locations()
-        food_locations = tank.get_locations(tank.food)
-        for dx, dy in possible_moves:
-            new_x = self.x + dx
-            new_y = self.y + dy
-            if 1 <= new_x < tank.width - 1 and 1 <= new_y < tank.height - 1 and (new_x, new_y) not in (occupied_locations - food_locations):
-                distance = abs(target_x - new_x) + abs(target_y - new_y)
-                fish_neighbor_count = sum([1 for fish in tank.fish if max(abs(new_x - fish.x), abs(new_y - fish.y)) <= 4])
-
-                if distance < best_distance or (distance == best_distance and fish_neighbor_count > best_fish_neighbor_count):
-                    best_move = (dx, dy)
-                    best_distance = distance
-                    best_fish_neighbor_count = fish_neighbor_count
+        for new_x, new_y in possible_moves:
+            distance = abs(target_x - new_x) + abs(target_y - new_y)
+            if distance > best_distance:
+                continue
+            fish_neighbor_count = 0
+            for x in range(max(new_x - 4, 1), min(new_x + 4, tank.width - 2)):
+                for y in range(max(new_y - 4, 1), min(new_y + 4, tank.height - 2)):
+                    if isinstance(tank.grid[x][y], Fish) and max(abs(new_x - x), abs(new_y - y)) <= 4:
+                        fish_neighbor_count += 1
+            if distance < best_distance or fish_neighbor_count > best_fish_neighbor_count:
+                best_move = new_x, new_y
+                best_distance = distance
+                best_fish_neighbor_count = fish_neighbor_count
 
         if best_move is not None:
-            self.x += best_move[0]
-            self.y += best_move[1]
-            for f in tank.food:
-                if (self.x, self.y) == f.get_pos():
-                    self.energy += 20
-                    tank.food.remove(f)
-                    break
+            if isinstance(tank.grid[best_move[0]][best_move[1]], Food):
+                self.energy += 20
+            tank.move_object(self, best_move[0], best_move[1])
 
     def can_reproduce(self) -> bool:
         reproduction_cooldown = self.base_reproduction_cooldown + self.age // 20
@@ -109,31 +108,32 @@ class Fish(TankObject):
             return
 
         possible_spawn_locations = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (-1, 1), (-1, -1), (1, -1)]
+        possible_spawn_locations = [(self.x + dx, self.y + dy) for dx, dy in possible_spawn_locations]
+        # Check if the baby fish's position is not occupied before adding it to the list of fish
+        possible_spawn_locations = [
+            (x, y) for x, y in possible_spawn_locations
+            if 1 <= x < tank.width - 1 and 1 <= y < tank.height - 1 and tank.grid[x][y] is None
+        ]
         random.shuffle(possible_spawn_locations)
 
-        for dx, dy in possible_spawn_locations:
-            baby_fish_x = self.x + dx
-            baby_fish_y = self.y + dy
-            # Check if the baby fish's position is not occupied before adding it to the list of fish
-            if 1 <= baby_fish_x < tank.width - 1 and 1 <= baby_fish_y < tank.height - 1 and (baby_fish_x, baby_fish_y) not in tank.get_occupied_locations():
-                # Check for a neighboring fish to reproduce with
-                neighbor_fish = None
-                for fish in tank.fish:
-                    if max(abs(baby_fish_x - fish.x), abs(baby_fish_y - fish.y)) == 1 and fish.can_reproduce():
-                        neighbor_fish = fish
-                        break
+        for baby_fish_x, baby_fish_y in possible_spawn_locations:
+            # Check for a neighboring fish to reproduce with
+            neighbor_locations = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (-1, 1), (-1, -1), (1, -1)]
+            neighbor_locations = [(baby_fish_x + dx, baby_fish_y + dy) for dx, dy in neighbor_locations]
+            neighbor_fish: list[Fish] = [tank.grid[x][y] for x, y in neighbor_locations if isinstance(tank.grid[x][y], Fish)]
+            random.shuffle(neighbor_fish)
 
-                if neighbor_fish is not None:
-                    # Create a new fish of the same type as the parent fish
-                    baby_fish = type(self)(baby_fish_x, baby_fish_y)
-                    baby_fish.energy = 10
-                    tank.fish.append(baby_fish)
-                    tank.fish_births += 1  # Increment the fish_births counter
-                    self.energy -= 5
-                    neighbor_fish.energy -= 5
-                    self.turns_since_last_reproduction = 0
-                    neighbor_fish.turns_since_last_reproduction = 0
-                    break
+            if neighbor_fish:
+                # Create a new fish of the same type as the parent fish
+                baby_fish = type(self)(baby_fish_x, baby_fish_y)
+                baby_fish.energy = 10
+                tank.add_object(baby_fish)
+                tank.fish_births += 1  # Increment the fish_births counter
+                self.energy -= 5
+                neighbor_fish[0].energy -= 5
+                self.turns_since_last_reproduction = 0
+                neighbor_fish[0].turns_since_last_reproduction = 0
+                break
 
 
 class SchoolingFish(Fish):
@@ -142,12 +142,12 @@ class SchoolingFish(Fish):
 
     def choose_target(self, tank: 'Tank'):
         if self.energy < self.energy_required_for_reproduction:
-            target_x, target_y = self.find_closest(tank.food)
+            target_x, target_y = self.find_closest(tank.grid, Food)
             # Find nearest fish if we can't find food
             if target_x is None:
-                target_x, target_y = self.find_closest(tank.fish)
+                target_x, target_y = self.find_closest(tank.grid, Fish)
         else:
-            target_x, target_y = self.find_closest(tank.fish)
+            target_x, target_y = self.find_closest(tank.grid, Fish)
         return target_x, target_y
 
 
@@ -183,19 +183,21 @@ class Tank:
         self.height = terminal_size.lines - 5
         # Parameters
         self.max_population = math.ceil(self.width * self.height * 0.02)
-        self.max_food = 200
         self.new_food_interval = 10
         self.new_food_amount = math.ceil(self.width/15)
 
         # Create a list of all possible locations and shuffle it
         all_locations = list(product(range(1, self.width - 1), range(1, self.height - 1)))
         random.shuffle(all_locations)
-
+        # Create a grid and add objects
+        self.grid = [[None for _ in range(self.height)] for _ in range(self.width)]
         # Assign locations to fish, food, and rocks, ensuring no two objects share the same location
-        self.rocks = self.create_rocks(all_locations)
-        self.fish = [SchoolingFish(*all_locations.pop()) for _ in range(math.ceil(self.width * self.height * 0.02)) if len(all_locations)]
-        self.dead_fish = []
-        self.food = [Food(*all_locations.pop()) for _ in range(math.ceil(self.width * self.height * 0.01)) if len(all_locations)]
+        self.add_rocks(all_locations)
+        fish = [SchoolingFish(*all_locations.pop()) for _ in range(math.ceil(self.width * self.height * 0.02)) if len(all_locations)]
+        food = [Food(*all_locations.pop()) for _ in range(math.ceil(self.width * self.height * 0.01)) if len(all_locations)]
+        # Add objects to the grid
+        for obj in fish + food:
+            self.add_object(obj)
 
         # Initialize statistics attributes
         self.iterations = 0
@@ -203,7 +205,28 @@ class Tank:
         self.fish_deaths = 0
         self.oldest_fish_ever = 0
 
-    def create_rocks(self, all_locations):
+    def add_object(self, obj: TankObject):
+        self.grid[obj.x][obj.y] = obj
+
+    def move_object(self, obj: TankObject, new_x: int, new_y: int):
+        self.grid[obj.x][obj.y] = None
+        self.grid[new_x][new_y] = obj
+        obj.x = new_x
+        obj.y = new_y
+
+    @property
+    def fish(self) -> list[Fish]:
+        return [obj for obj in chain(*self.grid) if isinstance(obj, Fish)]
+
+    @property
+    def food(self) -> list[Food]:
+        return [obj for obj in chain(*self.grid) if isinstance(obj, Food)]
+
+    @property
+    def dead_fish(self) -> list[DeadFish]:
+        return [obj for obj in chain(*self.grid) if isinstance(obj, DeadFish)]
+
+    def add_rocks(self, all_locations: set) -> list[Rock]:
         rocks = []
         n_rocks = math.ceil(self.width * self.height * 0.04)
         # Cache the neighbor counts for all locations
@@ -216,7 +239,7 @@ class Tank:
                 chosen_location = random.choices(all_locations, weights, k=1)[0]
                 all_locations.remove(chosen_location)
                 rock_locations.add(chosen_location)
-                rocks.append(Rock(*chosen_location))
+                self.add_object(Rock(*chosen_location))
 
                 # Update neighbor counts for the chosen location's neighbors
                 neighbors = [(chosen_location[0] + dx, chosen_location[1] + dy) for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]]
@@ -224,16 +247,6 @@ class Tank:
                     if neighbor in neighbor_counts:
                         neighbor_counts[neighbor] += 1
         return rocks
-
-    def get_occupied_locations(self, return_set: bool = True) -> set[Tuple[int, int]]:
-        locs = [tank_object.get_pos() for tank_object in self.get_all_objects()]
-        if return_set:
-            return set(locs)
-        else:
-            return locs
-
-    def get_all_objects(self) -> list[TankObject]:
-        return self.fish + self.food + self.rocks + self.dead_fish
 
     def average_fish_energy(self) -> float:
         if not self.fish:
@@ -243,29 +256,25 @@ class Tank:
     def get_max_age(self) -> int:
         return max(fish.age for fish in self.fish) if self.fish else 0
 
-    def get_locations(self, tank_objects: list[TankObject]) -> set[Tuple[int, int]]:
-        return set([f.get_pos() for f in tank_objects])
-
     def display(self):
-        # Check for overlapping TankObjects
-        if len(self.get_occupied_locations(return_set=True)) != len(self.get_occupied_locations(return_set=False)):
-            raise ValueError("Overlapping TankObjects detected.")
-
         # Create a grid containing the string to print for each location
-        grid = [[" " for _ in range(self.width)] for _ in range(self.height)]
-        for obj in self.get_all_objects():
-            x, y = obj.get_pos()
-            grid[y][x] = str(obj)
+        char_grid = []
         for y in range(self.height):
+            row = []
             for x in range(self.width):
                 if x == 0 or x == self.width - 1 or y == 0 or y == self.height - 1:
-                    grid[y][x] = Fore.WHITE + "+" + Style.RESET_ALL
+                    row.append(Fore.WHITE + "+" + Style.RESET_ALL)
+                elif self.grid[x][y] is None:
+                    row.append(" ")
+                else:
+                    row.append(str(self.grid[x][y]))
+            char_grid.append(row)
 
         if self.get_max_age() > self.oldest_fish_ever:
             self.oldest_fish_ever = self.get_max_age()
 
         # Create a string for the tank including a status
-        output = "\n".join(["".join(row) for row in grid])
+        output = "\n".join(["".join(row) for row in char_grid])
         output += "\n"
         output += f"Number of fish: {len(self.fish):5}      Fish births:    {self.fish_births:5}      Max Age:          {self.get_max_age():5}\n"
         output += f"Number of food: {len(self.food):5}      Fish deaths:    {self.fish_deaths:5}      Oldest fish ever: {self.oldest_fish_ever:5}\n"
@@ -276,30 +285,28 @@ class Tank:
         print(output, end="")
 
     def run(self):
-        all_objects = self.get_all_objects()
+        all_objects: list[TankObject] = [obj for obj in chain(*self.grid) if obj is not None and not isinstance(obj, Rock)]
         random.shuffle(all_objects) # Shuffle to increase randomness in the model
         for obj in all_objects:
             obj.run(self)
 
         new_dead_fish = [f for f in self.fish if f.is_dead()]
         for df in new_dead_fish:
-            self.fish.remove(df)
+            self.add_object(DeadFish(df.x, df.y))
             self.fish_deaths += 1  # Increment the fish_deaths counter
-            self.dead_fish.append(DeadFish(df.x, df.y))
 
         decomposed_fish = [df for df in self.dead_fish if df.is_decomposed()]
         for df in decomposed_fish:
-            self.dead_fish.remove(df)
-            self.food.append(Food(df.x, df.y))
+            self.add_object(Food(df.x, df.y))
 
         # Give new food every so often, don't allow more than the max food, and don't feed the fish if there are too many
-        if self.iterations % self.new_food_interval == 0 and len(self.food) < self.max_food and len(self.fish) < self.max_population:
+        if self.iterations % self.new_food_interval == 0 and len(self.fish) < self.max_population:
+            x_locations = [x for x in range(1, self.width - 1) if not self.grid[x][1]]
             for _ in range(self.new_food_amount):
-                food_x = random.randint(1, self.width - 2)
-                food_y = 1
-                while (food_x, food_y) in self.get_occupied_locations():
-                    food_x = random.randint(1, self.width - 2)
-                self.food.append(Food(food_x, food_y))
+                if x_locations:
+                    food_x = random.choice(x_locations)
+                    self.add_object(Food(food_x, 1))
+                    x_locations.remove(food_x)
 
         self.iterations += 1
 
@@ -309,9 +316,12 @@ def main():
     tank = Tank()
     os.system('cls' if os.name == 'nt' else 'clear')
     while True:
+        duration = time.time()
         tank.run()
         tank.display()
-        time.sleep(0.1)
+        duration = time.time() - duration
+        if duration < 0.1:
+            time.sleep(0.1 - duration)
 
 
 if __name__ == "__main__":
